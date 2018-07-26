2acae2a00d51bc2d3f823d51adfa35d0d442616d
luoyangxiaobird <32297572@qq.com>
2018-07-26 16:50:05 +0800

    test

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.default')
 @section('title','注册')
 @section('content')
-    <h1>注册开始</h1>
+    <div class="col-md-offset-2 col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h1>注册开始</h1>
+            </div>
+            <div class="panel-body">
+                <form method="post" action="{{route('users.store')}}">
+
+                    <div class="form-group">
+                        <label for="name">名称：</label>
+                        <input type="text" name="name" class="form-control" value="{{old('name')}}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">邮箱：</label>
+                        <input type="text" name="email" class="form-control" value="{{old('email')}}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">密码：</label>
+                        <input type="password" name="password" class="form-control" value="{{old('password')}}">
+                    </div>
+
+                    <div class="form-group">
+                        <label>确认密码：</label>
+                        <input type="password" name="password-confirmation" class="form-control" value="{{old('password-confirmation')}}">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">注册</button>
+                </form>
+            </div>
+        </div>
+    </div>//全局辅助函数old来帮助我们在blade中显示旧输入数据。
+
+
 @stop
